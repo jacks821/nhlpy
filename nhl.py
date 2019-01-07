e899d99bb1cffea0b5764891b2255d7fd4c053af
Charles Jackson <charlesjackson88@gmail.com>
2019-01-07 01:37:01 -0800

    updated the stats functions

--- nhl.py
+++ nhl.py
@@ -2,9 +2,16 @@ import requests
 import json
 from team import Team
 from player import Player
-from request import getPlayerSeasonStats
+from request import getPlayerSeasonStats, getPlayerMonthlySplit, getPlayerWinLossSplit, getPlayerDailySplit, getPlayerVsConference, getPlayerVsDivision, getPlayerVsTeam, getPlayerStatRanks
 
-y = getPlayerSeasonStats("Alex Tuch", 2018)
+#wl = getPlayerWinLossSplit("Alex Tuch", 2018)
+#month = getPlayerMonthlySplit("Alex Tuch", 2018)
+#daily = getPlayerDailySplit("Alex Tuch", 2018)
+#conf = getPlayerVsConference("Alex Tuch", 2018)
+#div = getPlayerVsDivision("Alex Tuch", 2018)
+#team = getPlayerVsTeam("Alex Tuch", 2018)
+#rank = getPlayerStatRanks("Alex Ovechkin", 2018)
+#stats = getPlayerSeasonStats("Alex Tuch", 2018)
 
-print(y)
+
 
--- request.py
+++ request.py
@@ -20,14 +20,70 @@ def findPlayer(playerName):
     return "Could not find player"
 
 
-#Gets player stats for given season.
+#Gets player stats for given season, with the options available.
 #Assumes that the year given is the latter calendar year in the season.
-def getPlayerSeasonStats(playerName, season):
+#Not meant to be publicly consumed as it assumes that only one of the options will be checked.
+def getPlayerSeasonStatswOpts(playerName, season, splits=False, wl=False, monthly=False, daily=False, division=False, conf=False, team=False, game=False, rank=False):
     player = findPlayer(playerName)
     s1 = season - 1
+    if wl == True:
+        req = player['link'] + "/stats" + "?stats=winLoss&season=" + str(s1) + str(season)
+        return request(req)['stats'][0]['splits']
+    if splits == True:
+        req = req = player['link'] + "/stats" + "?stats=homeAndAway&season=" + str(s1) + str(season)
+        return request(req)['stats'][0]['splits']
+    if monthly == True:
+        req = req = player['link'] + "/stats" + "?stats=byMonth&season=" + str(s1) + str(season)
+        return request(req)['stats'][0]['splits']
+    if daily == True:
+        req = req = player['link'] + "/stats" + "?stats=byDayOfWeek&season=" + str(s1) + str(season)
+        return request(req)['stats'][0]['splits']
+    if division == True:
+        req = req = player['link'] + "/stats" + "?stats=vsDivision&season=" + str(s1) + str(season)
+        return request(req)['stats'][0]['splits']
+    if conf == True:
+        req = req = player['link'] + "/stats" + "?stats=vsConference&season=" + str(s1) + str(season)
+        return request(req)['stats'][0]['splits']
+    if team == True:
+        req = req = player['link'] + "/stats" + "?stats=vsTeam&season=" + str(s1) + str(season)
+        return request(req)['stats'][0]['splits']
+    if game == True:
+        req = req = player['link'] + "/stats" + "?stats=gameLog&season=" + str(s1) + str(season)
+        return request(req)['stats'][0]['splits']
+    if rank == True:
+        req = req = player['link'] + "/stats" + "?stats=regularSeasonStatRankings&season=" + str(s1) + str(season)
+        return request(req)['stats'][0]['splits']
     req = player['link'] + "/stats" + "?stats=statsSingleSeason&season=" + str(s1) + str(season)
     return request(req)['stats'][0]
-            
+
+
+def getPlayerSeasonStats(playerName, season):
+    return getPlayerSeasonStatswOpts(playerName, season)
+
+def getPlayerHomeAwaySplit(playerName, season):
+    return getPlayerSeasonStatswOpts(playerName, season, splits=True)
+
+def getPlayerMonthlySplit(playerName, season):
+    return getPlayerSeasonStatswOpts(playerName, season, monthly=True)
+
+def getPlayerVsTeam(playerName, season):
+    return getPlayerSeasonStatswOpts(playerName, season, team=True)
+
+def getPlayerVsConference(playerName, season):
+    return getPlayerSeasonStatswOpts(playerName, season, conf=True)
+
+def getPlayerVsDivision(playerName, season):
+    return getPlayerSeasonStatswOpts(playerName, season, division=True)
+
+def getPlayerStatRanks(playerName, season):
+    return getPlayerSeasonStatswOpts(playerName, season, rank=True)
+
+def getPlayerDailySplit(playerName, season):
+    return getPlayerSeasonStatswOpts(playerName, season, daily=True)
+
+def getPlayerWinLossSplit(playerName, season):
+    return getPlayerSeasonStatswOpts(playerName, season, wl=True)
+
     
     
     
